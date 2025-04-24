6a516d9357a5928f2e0abc663c30542e98cb00b2
karinatabares1 <anakarinatabarescamargo707@gmail.com>
2025-04-24 13:25:55 -0500

    creacion de carpetas y comienzo de estilos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Header } from './Components/Layouts/Header/Header'
+
+export const App = () => {
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+
---
+++ src/Components/Layouts/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1 className='TituloPrincipal'>
+        🛒 Productos
+      </h1>
+    </header>
+  )
+}
